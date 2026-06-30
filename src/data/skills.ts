774d126e63ed368178827409810dd3cf451b244e
can-tls <talascan@googlemail.com>
2026-06-30 12:14:17 +0200

    Feature: Skills angelegt und level display (Sidebar)

---
+++ src/data/skills.ts
@@ -0,0 +1,30 @@
+export const skills = [
+    {
+        name: "C#",
+        level: 4
+    },
+    {
+        name: "React",
+        level: 3
+    },
+    {
+        name: "Angular",
+        level: 3
+    },
+    {
+        name: "TypeScript",
+        level: 3
+    },
+    {
+        name: "Ruby",
+        level: 3
+    },
+    {
+        name: "MongoDB",
+        level: 2
+    },
+    {
+        name: "PostgreSQL",
+        level: 2
+    }
+]
